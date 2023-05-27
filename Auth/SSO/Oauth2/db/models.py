48bfe1a436a7ee88f5e1c95cbd4e6dff6a929604
parsariyahi <pany.parsariyahi@gmail.com>
2023-05-27 23:05:29 +0330

    Add faker for inserting data and a basic select query

--- Auth/SSO/Oauth2/db/models.py
+++ Auth/SSO/Oauth2/db/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy import String, Boolean, create_engine
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import String, Boolean, create_engine, select
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, Session
 
 
 class Base(DeclarativeBase):
@@ -14,7 +14,28 @@ class User(Base):
     active: Mapped[bool] = mapped_column(Boolean())
     password: Mapped[str] = mapped_column(String(30))
 
+
 engine = create_engine("sqlite:///Auth/SSO/Oauth2/db/database.db", echo=True)
-
-
 Base.metadata.create_all(engine)
+
+def insert_fake_data():
+    with Session(engine) as session :
+        user1 = User(
+            username="parsa",
+            full_name="parsa riyahi",
+            active=True,
+            password="parsa1234"
+        )
+
+        session.add(user1)
+        session.commit()
+
+def select_fake_data() :
+    with Session(engine) as session:
+        query = select(User).where(User.username.in_(["parsa"]))
+
+        for user in session.scalars(query):
+            print(user.id)
+
+insert_fake_data()
+select_fake_data()
