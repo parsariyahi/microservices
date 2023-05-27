7becfeb7a2c615a3245a2b3c09d0df7559621d97
parsariyahi <pany.parsariyahi@gmail.com>
2023-05-27 23:29:12 +0330

    Add test function for ``pydantic``

--- Auth/SSO/Oauth2/db/schemas.py
+++ Auth/SSO/Oauth2/db/schemas.py
@@ -8,16 +8,28 @@ We can use this models in both database, Json raw data.
 
 class userSchema(BaseModel):
     username: str
-    email: str
     full_name: str
     active: bool
 
-class User(BaseModel):
-    username: str
-    email: str | None = None
-    full_name: str | None = None
-    disabled: bool | None = None
+# class User(BaseModel):
+#     username: str
+#     email: str | None = None
+#     full_name: str | None = None
+#     disabled: bool | None = None
 
 
-class UserInDB(User):
-    hashed_password: str
+class UserInDB(userSchema):
+    password: str
+
+
+def test_user_schema():
+    user = UserInDB(
+        username="parsa",
+        full_name="parsa riyahi",
+        active=True,
+        password="parsa1234",
+    )
+
+    print(*user.dict().values())
+
+test_user_schema()
